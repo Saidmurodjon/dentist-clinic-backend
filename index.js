a0f0d9aedd40ecfb9dc63583fac8367eeaf78825
kali <kali@kali.kali>
2022-08-07 17:22:10 +0500

    updated

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const appRouter = require("./router");
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const { MONGODB, PORT } = require("./config");
 
 app.use(express.json());
 app.use(cors());
 
-const url = `mongodb+srv://theBest:1020@cluster0.m79kn.mongodb.net/dentist`;
 
 const connectionParams = {
   useNewUrlParser: true,
@@ -16,7 +15,7 @@ const connectionParams = {
   useUnifiedTopology: true,
 };
 mongoose
-  .connect(url, connectionParams)
+  .connect(MONGODB, connectionParams)
   .then(() => {
     console.log("Connected to database ");
   })
@@ -26,6 +25,6 @@ mongoose
 
 app.use("/", appRouter);
 
-app.listen(port, () => {
-  console.log("5000 port working");
+app.listen(PORT, () => {
+  console.log(`${PORT}'s port is working`);
 });
